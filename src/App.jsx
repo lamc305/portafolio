ada65ded04cffbd5bba1372fcdfb5de8b30ada98
Luis Medina <lamc3005@gmail.com>
2022-10-11 14:23:31 -0400

    delete some sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css'
 import AboutMe from './components/AboutMe'
-import ContactMe from './components/ContactMe'
-import Experience from './components/Experience'
 import Nav from './components/Nav'
 import Portada from './components/Portada'
 import Projects from './components/Projects'
@@ -16,9 +14,7 @@ function App() {
         <Nav />
         <Portada />
         <AboutMe />
-        <Experience />
         <Projects />
-        <ContactMe />
       </IntersectionObserverProvider>
     </div>
   )
