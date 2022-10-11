acaaaeb805a71717e9f37ae396e8e116bf4b597c
Luis Medina <lamc3005@gmail.com>
2022-10-11 14:23:59 -0400

    apply rootMargin to intersection observer

--- src/context/IntersectionObserver.jsx
+++ src/context/IntersectionObserver.jsx
@@ -9,26 +9,23 @@ export function IntersectionObserverProvider({ children }) {
 
   const [toggleActive, setToggleActive] = useState('portada')
   const [ref1, inView1] = useInView({
-    "rootMargin": "-250px"
+    "rootMargin": "-150px"
   })
   const [ref2, inView2] = useInView({
-    "rootMargin": "-250px"
+    "rootMargin": "-150px"
   })
   const [ref3, inView3] = useInView({
-    "rootMargin": "-250px"
+    "rootMargin": "-150px"
   })
-  const [ref4, inView4] = useInView({
-    "rootMargin": "-250px"
-  })
+
 
   useEffect(() => {
     if (inView1) setToggleActive('portada')
     if (inView2) setToggleActive('aboutMe')
-    if (inView3) setToggleActive('experience')
-    if (inView4) setToggleActive('projects')
-  }, [inView1, inView2, inView3, inView4, setToggleActive])
+    if (inView3) setToggleActive('projects')
+  }, [inView1, inView2, inView3, setToggleActive])
 
-  const data = { toggleActive, setToggleActive, ref1, ref2, ref3, ref4, inView1, inView2, inView3, inView4 }
+  const data = { toggleActive, setToggleActive, ref1, ref2, ref3, inView1, inView2, inView3 }
 
   return (
     <IntersectionObserver.Provider value={data}>
