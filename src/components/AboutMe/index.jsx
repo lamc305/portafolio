d82e3bb242848dbe0c23a5f0c3de466a7b3ecc41
Luis Medina <lamc3005@gmail.com>
2022-10-18 11:10:04 -0400

    change the img formatt of why-me

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -1,24 +1,23 @@
 import { useContext } from "react"
 import { IntersectionObserver } from "../../context/IntersectionObserver"
-import logo from '../../assets/why-me2.jpg'
+import logo from '../../assets/why-me2.webp'
 import './aboutMeStyles.css'
 
 import Skills from "../Skills"
 function AboutMe() {
 
-  const { ref2, inView2 } = useContext(IntersectionObserver)
-
+  const { ref2 } = useContext(IntersectionObserver)
 
   return (
     <section id='aboutMe' ref={ref2}>
-      <h2 className={`${inView2 ? 'animate__textAbout' : ' '}`} >About Me</h2>
-      <div className={`${inView2 ? 'aboutMe__container animate__textAbout ' : 'aboutMe__container'}`}>
+      <h2>About Me</h2>
+      <div className='aboutMe__container'>
         <div >
           <img className='aboutMe__img' src={logo} alt="why me?" />
         </div>
         <div className='aboutMe__text'
         >
-          <p>I'm a <span className='red'>frontend developer with ReactJS</span>, able to carry out the designs that are presented to me and solving the problems that arise as the design is transferred to code </p>
+          <p>I'm a <span className='blue'>frontend developer with ReactJS</span>, able to carry out the designs that are presented to me and solving the problems that arise as the design is transferred to code </p>
           <p>My Skills are</p>
           <div className='skills__container'>
             <Skills />
