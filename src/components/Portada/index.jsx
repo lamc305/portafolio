5f2953deca61a28a7a2e8de6e0220db226035385
Luis Medina <lamc3005@gmail.com>
2022-11-27 19:10:37 -0400

    update the cv

--- src/components/Portada/index.jsx
+++ src/components/Portada/index.jsx
@@ -6,76 +6,89 @@ import { useContext } from 'react'
 import { IntersectionObserver } from '../../context/IntersectionObserver'
 import { FormattedMessage } from 'react-intl'
 import { LangContext } from '../../context/langContext'
-import cvSpanish from '../../assets/CV-Luis-Medina-Spanish.pdf'
-import cvEnglish from '../../assets/CV-Luis-Medina-English.pdf'
+import cvSpanish from '../../assets/CV-Luis-Medina-Desarrollador-Frontend.pdf'
+import cvEnglish from '../../assets/CV-Luis-Medina-Frontend-Developer.pdf'
 
 function Portada() {
+	const { ref1 } = useContext(IntersectionObserver)
+	const { locale } = useContext(LangContext)
+	const sendMessage = () => {
+		window.location.href = 'mailto:lamc3005@gmail.com'
+	}
 
-  const { ref1 } = useContext(IntersectionObserver)
-  const { locale } = useContext(LangContext)
-  const sendMessage = () => {
-    window.location.href = 'mailto:lamc3005@gmail.com'
-  }
-
-  return (
-    <section id='portada' ref={ref1}>
-      <div className='portada__container'>
-        <div className='portada__text'>
-          <p>
-            <FormattedMessage
-              id='app.hi'
-              defaultMessage='Hi there'
-            />
-          </p>
-          <h1>
-            <FormattedMessage
-              id='app.name'
-              defaultMessage="I'm Luis Medina"
-              values={{
-                name: <span className="blue">Luis Medina,</span>
-              }}
-            />
-          </h1>
-          <h2>
-            <FormattedMessage
-              id='app.job'
-              defaultMessage='I am a Frontend Developer'
-            />
-          </h2>
-          <div className='portada__buttons' >
-            <a
-              href={locale === 'en-US' ? cvEnglish : cvSpanish}
-              target='_blank'
-              className='btn'
-            >
-              <p>
-                <FormattedMessage
-                  id='portada.cv'
-                  defaultMessage='Resume'
-                />
-              </p> <AiOutlineDownload />
-            </a>
-            <button onClick={sendMessage} className='btn'>
-              <p>
-                <FormattedMessage
-                  id='portada.contact'
-                  defaultMessage='Contact me'
-                />
-              </p> <AiOutlineMail /></button>
-          </div>
-        </div>
-        <div className='hero__container'>
-          <img loading='lazy' className='hero' src={header2} alt="hero" />
-        </div>
-      </div>
-      <div className='links'>
-        <a aria-label="Read more about me in my profile on LinkedIn" href="https://www.linkedin.com/in/luis-medina-dev/" target="_blank" rel="noopener noreferrer"><BsLinkedin /></a>
-        <a aria-label="See more projects in my profile on Github" href="https://github.com/lamc305" target="_blank" rel="noopener noreferrer"><BsGithub /></a>
-        <a aria-label="You can contact to me on Telegram" href="https://telegram.me/lamc1058" target="_blank" rel="noopener noreferrer" ><BsTelegram /></a>
-        <div></div>
-      </div>
-    </section>
-  )
+	return (
+		<section id='portada' ref={ref1}>
+			<div className='portada__container'>
+				<div className='portada__text'>
+					<p>
+						<FormattedMessage id='app.hi' defaultMessage='Hi there' />
+					</p>
+					<h1>
+						<FormattedMessage
+							id='app.name'
+							defaultMessage="I'm Luis Medina"
+							values={{
+								name: <span className='blue'>Luis Medina,</span>,
+							}}
+						/>
+					</h1>
+					<h2>
+						<FormattedMessage
+							id='app.job'
+							defaultMessage='I am a Frontend Developer'
+						/>
+					</h2>
+					<div className='portada__buttons'>
+						<a
+							href={locale === 'en-US' ? cvEnglish : cvSpanish}
+							target='_blank'
+							className='btn'>
+							<p>
+								<FormattedMessage id='portada.cv' defaultMessage='Resume' />
+							</p>{' '}
+							<AiOutlineDownload />
+						</a>
+						<button onClick={sendMessage} className='btn'>
+							<p>
+								<FormattedMessage
+									id='portada.contact'
+									defaultMessage='Contact me'
+								/>
+							</p>{' '}
+							<AiOutlineMail />
+						</button>
+					</div>
+				</div>
+				<div className='hero__container'>
+					<img loading='lazy' className='hero' src={header2} alt='hero' />
+				</div>
+			</div>
+			<div className='links'>
+				<a
+					aria-label='Read more about me in my profile on LinkedIn'
+					href='https://www.linkedin.com/in/luis-medina-dev/'
+					target='_blank'
+					rel='noopener noreferrer'>
+					<BsLinkedin />
+				</a>
+				<a
+					aria-label='See more projects in my profile on Github'
+					href='https://github.com/lamc305'
+					target='_blank'
+					rel='noopener noreferrer'>
+					<BsGithub />
+				</a>
+				<a
+					aria-label='You can contact to me on Telegram'
+					href='https://telegram.me/lamc1058'
+					target='_blank'
+					rel='noopener noreferrer'>
+					<BsTelegram />
+				</a>
+				<div></div>
+			</div>
+		</section>
+	)
 }
 
 export default Portada
